f72ec8d174a141519e07c830d43b608507de60ca
josoriom <javier.osorio@correounivalle.edu.co>
2021-05-28 22:55:46 -0500

    fix: bug in getSampleIDs function

--- src/utilities/getSamplesIDs.js
+++ src/utilities/getSamplesIDs.js
@@ -3,24 +3,19 @@
  * @returns {Array}
  */
 export function getSamplesIDs(wells) {
-    let sampleWells = [];
-    let sampleIDs = [];
-    for (let i = 0; i < wells.length; i++) {
-        let replicated = JSON.stringify(
-        wells[i].reagents.map((item) => item.concentration),
-        );
-        let feature = sampleWells.find((element) => element === replicated);
-        if (feature + 1) continue;
-        sampleWells.push(replicated);
-        const replicates = wells
-        .filter(
-            (item) =>
-            JSON.stringify(
-                item.reagents.map((element) => element.concentration),
-            ) === replicated,
-        )
-        .map((item) => item.id);
-        sampleIDs.push(replicates);
-    }
-    return sampleIDs;
+  let sampleWells = [];
+  let sampleIDs = [];
+  for (let i = 0; i < wells.length; i++) {
+    let replicated = JSON.stringify(
+      wells[i].reagents.map((item) => item.concentration),
+    );
+    let feature = sampleWells.find((element) => element === replicated);
+    if (feature + 1) continue;
+    sampleWells.push(replicated);
+    const replicates = wells
+      .filter((item) => JSON.stringify(item.reagents.map((element) => element.concentration)) === replicated)
+      .map((item) => item.id);
+    sampleIDs.push(replicates);
+  }
+  return sampleIDs;
 }
